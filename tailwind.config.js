d6c632a32f24b986210395c090b0ebeb4ac4a403
مَرۡيَمَ مُوسَى <mariam.mm2636934@gmail.com>
2024-11-28 12:08:12 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        primary: {
+          "50": "#d5d8dc",
+          "100": "#c0c5cb",
+          "200": "#abb2b9",
+          "300": "#19AB8E",
+          "400": "#1ABC9C",
+          "500": "#6b7885",
+          "600": "#566573",
+          "700": "#283848",
+          "800": "#2C3E50",
+        }
+      },
+      screens: {
+        "2xl": "1320px"
+      }
+    },
+  },
+  plugins: [],
+}
+
